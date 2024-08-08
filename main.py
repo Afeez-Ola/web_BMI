4b997781f5951072af6ebc0d8df17936aa137384
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-08-08 15:58:30 +0200

    Refactoring

--- main.py
+++ main.py
@@ -6,29 +6,32 @@ os.getcwd()
 app = Flask(__name__)
 
 
-def getInputs():
-    bill_amount = float(input("What was the total bill: "))
-    tip_percentage = float(input("What is the total tip: "))
-    number_of_people = int(input("How many people to split with: "))
+# def getInputs():
+#     bill_amount = float(input("What was the total bill: "))
+#     tip_percentage = float(input("What is the total tip: "))
+#     number_of_people = int(input("How many people to split with: "))
+#
+#     return tip_percentage, bill_amount, number_of_people
 
-    return tip_percentage, bill_amount, number_of_people
 
-
-def tip_calculations():
-    tip_percentage, bill_amount, number_of_people = getInputs()
-
-    total_tip = bill_amount * (tip_percentage / 100)
-    total_bill = total_tip + bill_amount
-    return total_bill,total_tip
+def tip_calculations(tip_percentage, bill_amount, number_of_people):
+    total_tip = bill_amount * (float(tip_percentage) / 100)
+    total_bill = float(total_tip + bill_amount)
+    amount_per_person = float(total_bill / number_of_people)
+    return total_bill, total_tip, amount_per_person
 
 
 @app.route("/", methods=["GET", "POST"])
 def main():
     result = None
     if request.method == "POST":
-        result = "We are up"
-    return render_template("index.html", )
+        bill_amount = request.form["bill-amount"]
+        tip_percentage = request.form["tip-percentage"]
+        number_of_people = request.form["number-of-people"]
 
+        total_bill, total_tip, amount_per_person = tip_calculations(tip_percentage, bill_amount, number_of_people)
+
+    return render_template("index.html", )
 
 
 if __name__ == "__main__":
