7aed531756406fb224ebe907282e70de32730326
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-08-08 15:40:31 +0200

    Refactoring

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ os.getcwd()
 
 app = Flask(__name__)
 
+
 def getInputs():
     total_bill = float(input("What was the total bill: "))
     total_tip = float(input("What is the total tip: "))
     number_of_people = int(input("How many people to split with: "))
+
+    return total_tip,total_bill,number_of_people
+
+def tip_calculations():
+    total_tip, total_bill, number_of_people = getInputs()
 
 
 @app.route("/", methods=["GET", "POST"])
