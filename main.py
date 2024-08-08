77a2b386bba4c445733a12c229b1a046fedb5983
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-08-08 15:43:58 +0200

    Refactoring

--- main.py
+++ main.py
@@ -11,21 +11,24 @@ def getInputs():
     tip_percentage = float(input("What is the total tip: "))
     number_of_people = int(input("How many people to split with: "))
 
-    return tip_percentage,bill_amount,number_of_people
+    return tip_percentage, bill_amount, number_of_people
+
 
 def tip_calculations():
     tip_percentage, bill_amount, number_of_people = getInputs()
 
     total_tip = bill_amount * (tip_percentage / 100)
     total_bill = total_tip + bill_amount
-    return total_bill
+    return total_bill,total_tip
+
 
 @app.route("/", methods=["GET", "POST"])
 def main():
     result = None
-    if request.method == "GET":
+    if request.method == "POST":
         result = "We are up"
-    return result
+    return render_template("index.html", )
+
 
 
 if __name__ == "__main__":
